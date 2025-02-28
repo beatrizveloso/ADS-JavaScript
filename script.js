2be3dc8f249d607e6510ec9328d6cb699df15caa
Beatriz Veloso <beatrizoslov6@gmail.com>
2025-02-28 10:25:14 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+// Exercícios da aula do Prof Luís 
+
+// 1 - Soma de dois números
+function soma(){
+ 
+    var n1 = parseInt(document.getElementById('num1').value)
+    var n2 = parseInt(document.getElementById('num2').value); ; 
+    var soma = n1 + n2;
+    document.getElementById('res').value = soma;
+ }
+ 
+// 2 - Verificar se um número é par ou ímpar
+let numero = 7;
+if (numero % 2 === 0) {
+  console.log("par");
+} else {
+  console.log("ímpar");
+}
+
+// 3 - Tabuada de um número
+ 
+// 4 - Inverter uma string
+
+// 5 - Calcular o fatorial de um número
+
+// 6 - Gerar um número aleatório entre 1 e 100
+
+// 7 - Verificar se uma palavra é um palíndromo
+ 
+// 8 - Encontrar o maior número em um array
+
+// 9 - Contar vogais de em uma string
+
+// 10 - Soma dos números de um array
